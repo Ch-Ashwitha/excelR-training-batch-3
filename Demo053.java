1dc32787e22319d29c773f60d71c23a2dff13e43
Ch-Ashwitha <ashwitha11ashu@gmail.com>
2025-01-25 12:56:47 +0530

    to check number is strong or not

---
+++ Demo053.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Demo053 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = scanner.nextInt();
+        int originalNum = num;
+        int sum = 0;
+        
+        while (num != 0) {
+            int digit = num % 10;
+            int fact = 1;
+            for (int i = 1; i <= digit; i++) {
+                fact *= i;
+            }
+            sum += fact;
+            num /= 10;
+        }
+        
+        if (sum == originalNum) {
+            System.out.println("Strong Number");
+        } else {
+            System.out.println("Not a Strong Number");
+}
+}
+}
