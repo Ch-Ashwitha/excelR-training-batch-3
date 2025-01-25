894a794a89973f0c44db1613cf94c5a325c2d7a1
Ch-Ashwitha <ashwitha11ashu@gmail.com>
2025-01-25 12:30:39 +0530

    to swap 2 numbers without 3rd variable

---
+++ Demo045.java
@@ -0,0 +1,11 @@
+class Demo045{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        a = a+b;
+        b = a-b;
+        a=a-b;
+        System.out.println(a);
+        System.out.println(b);
+        }
+}
