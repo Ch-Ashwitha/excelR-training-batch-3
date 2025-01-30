6f90c3600a40e553230845d0d71ffef34b562514
Ch-Ashwitha <ashwitha11ashu@gmail.com>
2025-01-30 07:39:50 +0530

    Update Demo025.java

--- Demo025.java
+++ Demo025.java
@@ -1,14 +1,11 @@
 public class Demo025 {
     public static void main(String[] args) {
-        String input = "Hello all happy sankranthi";
-        char[] charArray = input.toCharArray();
-        char[] reversedArray = new char[charArray.length];
-
-        for (int i = 0; i < charArray.length; i++) {
-            reversedArray[i] = charArray[charArray.length - 1 - i];
+        String str = "Hello all happy sankranthi";
+        String reversed = "";
+        for(int i = str.length()-1;i>=0;i--){
+            reversed+=str.charAt(i);
         }
-
-        String reversed = new String(reversedArray);
+        
         System.out.println("Reversed String: " + reversed);
     }
 }
