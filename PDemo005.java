ef9726f1ad32752e1ea41c6bcd87eee0202a260b
Ch-Ashwitha <ashwithareddyashu11@gmail.com>
2025-02-01 13:00:42 +0530

    student credentials

---
+++ PDemo005.java
@@ -0,0 +1,10 @@
+class PDemo005{
+    public static void main(String[] args){
+        String stu_name = "Ashwitha";
+        int stu_num = 142;
+        while(stu_name == "Ashwitha" && stu_num == 142) {
+            System.out.println("WELCOME");
+            break;
+        }
+    }
+}
