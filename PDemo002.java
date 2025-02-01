552bd790c43cd81d69c61273f6cf7e8bb45cd2aa
Ch-Ashwitha <ashwithareddyashu11@gmail.com>
2025-02-01 12:29:05 +0530

    simple if else

---
+++ PDemo002.java
@@ -0,0 +1,12 @@
+class PDemo002{
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 18;
+        if (num1 > num2){
+            System.out.println(num1 + " is greater than " + num2);
+        }else  {
+            System.out.println(num2 + " is greater than " + num1);
+
+        }
+    }
+}
