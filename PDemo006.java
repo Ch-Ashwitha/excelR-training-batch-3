f9bce44fa22052665fb8af4254db6d4619a22f10
Ch-Ashwitha <ashwithareddyashu11@gmail.com>
2025-02-01 13:01:15 +0530

    simple while loop

---
+++ PDemo006.java
@@ -0,0 +1,10 @@
+class PDemo006{
+    public static void main(String[] args){
+        String name = "Ashwitha";
+        int dob = 11-02-2005;
+        while(name == "Ashwitha" && dob == 11-02-2005) {
+            System.out.println("HAPPY BIRTHDAY!!");
+            break;
+        }
+    }
+}
